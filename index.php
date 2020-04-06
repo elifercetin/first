68b05937c5bc571a1a64a9ae316acca4ebfd5670
Elif Erçetin <elifercetin04@gmail.com>
2020-04-06 18:04:54 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CSS DERSİ</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="ders1.css">
+	<style>
+		
+		h1 {
+			color: red;
+		}
+	</style>
+</head>
+<body>
+	<h1>CSS DERSLERİ</h1>
+	<P style="color:blue">ÖĞRENİYORM</P> 
+	<a href="">Buraya Tıklayın</a>
+
+
+</body>
+</html>
